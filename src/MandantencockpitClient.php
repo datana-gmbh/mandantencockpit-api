2dd54a389bc3fff3bb9b735c5c60502e4db4be47
Oskar Stark <oskarstark@googlemail.com>
2022-01-27 14:06:25 +0100

    Fix

--- src/MandantencockpitClient.php
+++ src/MandantencockpitClient.php
@@ -64,7 +64,7 @@ final class MandantencockpitClient
                 [
                     'query' => [
                         'token' => $this->token,
-                    ]
+                    ],
                 ]
             )
         );
